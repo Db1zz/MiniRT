971c421211fe37ce965d69235c3a5e62e445d66f
Gorg Onischenko <gonische@c2r1s2.42wolfsburg.de>
2024-11-23 16:28:03 +0100

    simplfy vector math functions

--- src/vector/vector_math1.c
+++ src/vector/vector_math1.c
@@ -3,34 +3,22 @@
 
 t_vector	vec3_div_vec3(t_vector v1, t_vector v2)
 {
-	v1.x = v1.x / v2.x;
-	v1.y = v1.y / v2.y;
-	v1.z = v1.z / v2.z;
-	return (v1);
+	return ((t_vector){v1.x / v2.x, v1.y / v2.y, v1.z / v2.z,});
 }
 
 t_vector	vec3_mult_vec3(t_vector v1, t_vector v2)
 {
-	v1.x = v1.x * v2.x;
-	v1.y = v1.y * v2.y;
-	v1.z = v1.z * v2.z;
-	return (v1);
+	return ((t_vector){v1.x * v2.x, v1.y * v2.y, v1.z * v2.z,});
 }
 
 t_vector	vec3_sub_vec3(t_vector v1, t_vector v2)
 {
-	v1.x = v1.x - v2.x;
-	v1.y = v1.y - v2.y;
-	v1.z = v1.z - v2.z;
-	return (v1);
+	return ((t_vector){v1.x - v2.x, v1.y - v2.y, v1.z - v2.z,});
 }
 
 t_vector	vec3_add_vec3(t_vector v1, t_vector v2)
 {
-	v1.x = v1.x + v2.x;
-	v1.y = v1.y + v2.y;
-	v1.z = v1.z + v2.z;
-	return (v1);
+	return ((t_vector){v1.x + v2.x, v1.y + v2.y, v1.z + v2.z,});
 }
 
 t_vector	vec3_normalize(t_vector vector)
