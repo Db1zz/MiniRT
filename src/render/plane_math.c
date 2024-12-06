10e3a50784e2f8f39b7edaebaa05fd0a614d9d1e
George Lukas Wagner <georgelukaswagner@gmail.com>
2024-12-06 14:33:45 +0100

    added plane math fr fr no cap

--- src/render/plane_math.c
+++ src/render/plane_math.c
@@ -7,17 +7,29 @@ double	fabs(double x)
 	return (x);
 }
 
-bool	ray_hit_plane(t_ray ray, t_plane plane)
+bool	ray_hit_plane(t_ray ray, t_object_list *plane_object, t_hit_record *rec)
 {
 	double denom;
 	double t;
+    t_plane *plane;
 
-	denom = vec3_dot(ray.dir, plane.normal);
+    if (plane_object->type != E_PLANE || plane_object->data == NULL)
+		return (false);
+    plane = (t_plane *)plane_object->data;
+	denom = vec3_dot(ray.direction, plane->vector);
 	if (fabs(denom) > 0.0001)
 	{
-		t = vec3_dot(vec3_sub_vec3(plane.point, ray.origin), plane.normal) / denom;
+		t = vec3_dot(vec3_sub_vec3(plane->point, ray.origin), plane->vector) / denom;
 		if (t >= 0)
+        {
+            if (denom > 0.0001) {
+                rec->front_face = true;
+            }
+            rec->t = t;
+            rec->normal = plane->vector;
+            rec->obj_type = E_PLANE;
 			return (true);
+        }
 	}
 	return (false);
 }
--- src/render/ray.c
+++ src/render/ray.c
@@ -1,4 +1,5 @@
 #include "ray.h"
+#include "light.h"
 
 t_ray	create_ray(t_vector origin, t_vector direction)
 {
@@ -6,21 +7,47 @@ t_ray	create_ray(t_vector origin, t_vector direction)
 }
 
 bool	ray_hit(const t_object_list *objects, const t_ray *ray,
-			const t_ray_properties *prop, t_hit_record *rec)
+			const t_ray_properties *prop, t_hit_record *result_rec)
 {
+	t_hit_record	current_rec;
+	bool			found;
+
+	found = false;
+	init_hit_record(&current_rec);
 	while (objects)
 	{
-		if (ray_hit_sphere(objects, ray, &prop->ray_interval, rec))
-			return (true);
+		if (ray_hit_sphere(objects, ray, &prop->ray_interval, &current_rec))
+			found = true;
+		if (ray_hit_plane(ray, objects, &current_rec))
+			found = true;
+		if (found)
+			*result_rec = get_closest_hit(&current_rec, result_rec);
 		objects = objects->next;
 	}
-	return (false);
+	return (found);
 }
+
+t_color	process_ray_hit(const t_object_list *objects,
+			const t_ray_properties *prop, const t_hit_record *rec)
+{
+	t_color	ray_color;
+	double	light_intensity;
+	
+	if (prop->light)
+	{
+		light_intensity = send_shadow_ray(objects, prop, rec);
+		ray_color = clr_mult(rec->color, light_intensity);
+	}
+	if (prop->amb_lighting)
+		ray_color = apply_ambient_light(prop->amb_lighting, &ray_color);
+	return (ray_color);
+}
+
 t_color	ray_send(const t_object_list *objects, const t_ray *ray,
-			t_ray_properties *prop, t_hit_record *rec)
+			const t_ray_properties *prop, t_hit_record *rec)
 {
 	if (prop->max_diffusion_depth > 0 && ray_hit(objects, ray, prop, rec))
-		return (diffuse_material(objects, prop, rec));
+		return (process_ray_hit(objects, prop, rec));
 	return (ray_get_background_color(ray));
 }
 
@@ -31,5 +58,6 @@ t_color	ray_color(const t_object_list *objects, const t_ray *ray,
 	t_ray_properties	prop_copy;
 
 	prop_copy = *prop;
+	init_hit_record(&rec);
 	return (ray_send(objects, ray, &prop_copy, &rec));
 }
