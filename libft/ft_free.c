c55a4631ef5c21a2fbfb1a861baa85b7aeacb9d9
Georgijs Onischenko <xel27@inbox.lv>
2025-04-29 22:17:37 +0200

    delete this

--- libft/ft_free.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-#include <stdlib.h>
-
-void	ft_free(void **data)
-{
-	free(data);
-	*data = NULL;
-}
--- libft/libft.h
+++ libft/libft.h
@@ -54,7 +54,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-void	ft_free(void **data);
 int		ft_rand(void);
 
 typedef struct s_list
