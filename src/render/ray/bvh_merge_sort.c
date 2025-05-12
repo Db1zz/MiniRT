e975f5df13e14d7f6ff5f4f815e234e9f8d3b49e
Georgijs Onischenko <xel27@inbox.lv>
2025-05-12 14:56:04 +0200

    norminette pt2

--- src/render/ray/bvh_merge_sort.c
+++ src/render/ray/bvh_merge_sort.c
@@ -1,23 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bvh_merge_sort.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gonische <gonische@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 14:45:40 by gonische          #+#    #+#             */
+/*   Updated: 2025/05/12 14:45:40 by gonische         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "bvh.h"
 
-static void merge(
+#define MERGE_SORT_BUFFER_MAX_SIZE 1024
+
+static void	copy_objects_from_buffer(
+	t_object **dest,
+	t_object **src,
+	size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+}
+
+static void	merge(
 	t_object **objects_start,
 	int left_size,
 	int right_size,
 	t_obj_comparator comparator)
 {
-	const int sorting_size = right_size + left_size;
-	t_object *buffer[left_size + right_size];
-	int i;
-	int j;
-	int k;
+	const int	sorting_size = right_size + left_size;
+	t_object	*buffer[MERGE_SORT_BUFFER_MAX_SIZE];
+	int			i;
+	int			j;
+	int			k;
 
 	if (left_size + right_size == 0)
-		return;
+		return ;
 	i = 0;
 	j = 0;
 	k = left_size;
-	while (j < left_size && k < right_size + left_size)
+	while (j < left_size && k < sorting_size)
 	{
 		if (comparator(objects_start[j], objects_start[k]))
 			buffer[i] = objects_start[j++];
@@ -29,27 +58,21 @@ static void merge(
 		buffer[i++] = objects_start[j++];
 	while (k < left_size + right_size)
 		buffer[i++] = objects_start[k++];
-	i = 0;
-	while (i < sorting_size)
-	{
-		objects_start[i] = buffer[i];
-		++i;
-	}
+	copy_objects_from_buffer(objects_start, buffer, sorting_size);
 }
 
-void merge_sort_objects_array(
+void	merge_sort_objects_array(
 	t_object **objects,
 	int start,
 	int end,
 	t_obj_comparator comparator)
 {
-	int mittel;
-	int left_size;
-	int right_size;
+	int	mittel;
+	int	left_size;
+	int	right_size;
 
 	if (start >= end || end - start == 0)
-		return;
-
+		return ;
 	mittel = start + (end - start) / 2;
 	left_size = mittel + 1 - start;
 	right_size = end - mittel;
