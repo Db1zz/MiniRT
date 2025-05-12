2d8c3e8c9af4590563583980ed64c3a4ff51b571
George Lukas Wagner <georgelukaswagner@gmail.com>
2025-05-12 17:12:13 +0200

    norminette update p8

--- include/ray_shapes.h
+++ include/ray_shapes.h
@@ -6,7 +6,7 @@
 /*   By: gwagner <gwagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 00:55:38 by gonische          #+#    #+#             */
-/*   Updated: 2025/05/12 16:56:54 by gwagner          ###   ########.fr       */
+/*   Updated: 2025/05/12 17:11:35 by gwagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,54 +16,54 @@
 # include "ray.h" /* ray | functions */
 # include "hit_record.h" /* t_hit_record */
 
-bool	ray_hit_shape(
-			const t_ray *ray,
-			const t_object *shape,
-			t_hit_record *result_rec);
+bool		ray_hit_shape(
+				const t_ray *ray,
+				const t_object *shape,
+				t_hit_record *result_rec);
 
-bool	ray_hit_aabb(
-			const t_ray *r,
-			const t_aabb *aabb);
+bool		ray_hit_aabb(
+				const t_ray *r,
+				const t_aabb *aabb);
 
-bool	ray_hit_multiple_shapes(
-			const t_ray *ray,
-			const t_object **shapes,
-			t_hit_record *result_rec);
+bool		ray_hit_multiple_shapes(
+				const t_ray *ray,
+				const t_object **shapes,
+				t_hit_record *result_rec);
 
-bool	ray_hit_sphere(
-			const t_ray *ray,
-			const t_object *sphere_object,
-			t_hit_record *rec);
+bool		ray_hit_sphere(
+				const t_ray *ray,
+				const t_object *sphere_object,
+				t_hit_record *rec);
 
-bool	ray_hit_plane(
-			const t_ray *ray,
-			const t_object *plane_object,
-			t_hit_record *rec);
+bool		ray_hit_plane(
+				const t_ray *ray,
+				const t_object *plane_object,
+				t_hit_record *rec);
 
-bool	ray_hit_cylinder(
-			const t_ray *ray,
-			const t_object *cylinder_object,
-			t_hit_record *rec);
+bool		ray_hit_cylinder(
+				const t_ray *ray,
+				const t_object *cylinder_object,
+				t_hit_record *rec);
 
-bool	ray_hit_gyper(
-			const t_ray *ray,
-			const t_object *gyper,
-			t_hit_record *rec);
+bool		ray_hit_gyper(
+				const t_ray *ray,
+				const t_object *gyper,
+				t_hit_record *rec);
 
 t_vector	calc_cylinder_normal(
-	const t_cylinder *cy,
-	const t_ray *ray,
-	double *s2,
-	double *dist);
+				const t_cylinder *cy,
+				const t_ray *ray,
+				double *s2,
+				double *dist);
 
-bool	find_cylinder_solutions(
-	const t_cylinder *cy,
-	const t_ray *ray,
-	double *s);
+bool		find_cylinder_solutions(
+				const t_cylinder *cy,
+				const t_ray *ray,
+				double *s);
 
-bool	tube_intersection(
-	const t_cylinder *cy,
-	const t_ray *ray,
-	t_hit_record *rec);
+bool		tube_intersection(
+				const t_cylinder *cy,
+				const t_ray *ray,
+				t_hit_record *rec);
 
 #endif // RAY_SHAPES_H
